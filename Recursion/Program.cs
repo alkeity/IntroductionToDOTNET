363d474c78f95b25a93d2ac8fb791d398697f423
alkeity <alkeity.23@gmail.com>
2024-06-19 18:36:20 +0300

    done: Factorial, Power

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -8,8 +8,23 @@ namespace Recursion
 {
 	internal class Program
 	{
+		static ulong Factorial(uint number)
+		{
+			if (number == 0 || number == 1) return 1;
+			return number * Factorial(number - 1);
+		}
+
+		static ulong Power(uint number, uint power)
+		{
+			if (power == 1) { return number; }
+			return number * Power(number, power - 1);
+		}
+
 		static void Main(string[] args)
 		{
+			uint num = 12, power = 6;
+			Console.WriteLine($"Факториал числа {num}: {Factorial(num)}");
+			Console.WriteLine($"{num} в {power} степени: {Power(num, power)}");
 		}
 	}
 }
