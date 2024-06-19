933285551393d384c02f3320a996ddf813a44f17
alkeity <alkeity.23@gmail.com>
2024-06-19 15:15:50 +0300

    simple calc done

--- Calc2/Program.cs
+++ Calc2/Program.cs
@@ -10,7 +10,40 @@ namespace Calc2
 	{
 		static void Main(string[] args)
 		{
-			
+			Console.Write("Введите простое арифметическое выражение: ");
+			string expression = Console.ReadLine();
+			//Console.WriteLine(expression);
+			expression = expression.Replace('.', ',');
+			String[] nums = expression.Split('+', '-', '*', '/');
+			//for (int i = 0; i < nums.Length; i++)
+			//{
+			//	Console.Write(nums[i] + ' ');
+			//}
+
+			double a = Convert.ToDouble(nums[0]);
+			double b = Convert.ToDouble(nums[1]);
+			char s = expression[expression.IndexOfAny(new char[]{ '+', '-', '*', '/' })];
+
+			//Console.WriteLine(a + " " + s + " " + b);
+
+			switch (s)
+			{
+				case '+':
+					Console.WriteLine($"{a} + {b} = {a + b}");
+					break;
+				case '-':
+					Console.WriteLine($"{a} - {b} = {a - b}");
+					break;
+				case '*':
+					Console.WriteLine($"{a} * {b} = {a * b}");
+					break;
+				case '/':
+					Console.WriteLine($"{a} / {b} = {a / b}");
+					break;
+				default:
+					break;
+			}
+			Main(args);
 		}
 	}
 }
