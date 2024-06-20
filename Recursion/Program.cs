71c9b28216576a74b9db7bbb368526af3c488914
alkeity <alkeity.23@gmail.com>
2024-06-20 14:26:58 +0300

    Fibonacci done

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -20,11 +20,42 @@ namespace Recursion
 				number * Power(number, power - 1);
 		}
 
+		static void Fibonacci(uint amount, uint position = 0, uint current = 0, uint next = 1)
+		{
+			Console.Write(current + " ");
+			if (++position < amount)
+			{
+				Fibonacci(amount, position, next, current + next);
+			}
+		}
+
+		static void Fibonacci(ulong limit, uint current = 0, uint next = 1)
+		{
+			Console.Write(current + " ");
+			if (next < limit)
+			{
+				Fibonacci(limit, next, current + next);
+			}
+		}
+
+		static ulong Fibonacci(uint position)
+		{
+			if (position <= 1) return position;
+			return Fibonacci(position - 1) + Fibonacci(position - 2);
+		}
+
 		static void Main(string[] args)
 		{
-			uint num = 12, power = 6;
+			uint num = 65, power = 15, fiboAmount = 120;
+			ulong fiboLimit = 5000;
 			Console.WriteLine($"Факториал числа {num}: {Factorial(num)}");
 			Console.WriteLine($"{num} в {power} степени: {Power(num, power)}");
+
+			Console.WriteLine($"Ряд Фибоначчи с пределом {fiboLimit}:");
+			Fibonacci(fiboLimit, 0, 1);
+			Console.WriteLine($"\nРяд Фибоначчи в количестве {fiboAmount} элементов:");
+			Fibonacci(fiboAmount, 0, 0, 1);
+			Console.WriteLine();
 		}
 	}
 }
