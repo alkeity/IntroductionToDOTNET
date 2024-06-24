03ae0744a495523b52d7a4dc73ce32e26d4ccc5e
alkeity <alkeity.23@gmail.com>
2024-06-24 16:29:15 +0300

    Recursion classwork (2)

--- RecursionClasswork/Program.cs
+++ RecursionClasswork/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace RecursionClasswork
 {
@@ -13,19 +14,26 @@ namespace RecursionClasswork
 			//Console.Write("Введите номер этажа: ");
 			//int floor = Convert.ToInt32(Console.ReadLine());
 			//Elevator(floor);
-			
-			try
-			{
-				for (int i = 0; i < 100; i++)
-				{
-					Console.WriteLine($"!{i} = {Factorial(i)}");
-				}
-			}
-			catch (Exception ex)
-			{
 
-				Console.WriteLine($"{ex.GetType()}: {ex.Message}");
-			}
+			//try
+			//{
+			//	for (int i = 0; i < 100; i++)
+			//	{
+			//		Console.WriteLine($"!{i} = {BigFactorial(i)}");
+			//	}
+			//}
+			//catch (Exception ex)
+			//{
+
+			//	Console.WriteLine($"{ex.GetType()}: {ex.Message}");
+			//}
+			long num = 1000000;
+			DateTime start = DateTime.Now;
+			Console.WriteLine($"!{num} = {BigFactorial(num)}");
+			DateTime end = DateTime.Now;
+			TimeSpan duration = end - start;
+
+			Console.WriteLine($"Duration = {duration.ToString("g")}");
 		}
 
 		static void Elevator(int floor)
@@ -38,6 +46,21 @@ namespace RecursionClasswork
 		static long Factorial(long number)
 		{
 			return number <= 1 ? 1 : number * Factorial(number - 1);
+		}
+
+		static BigInteger BigFactorial(long number)
+		{
+			BigInteger f = 1;
+			for (int i = 1; i < number; i++) { f *= i; }
+			return f;
+		}
+
+		static BigInteger Power(long number, int power)
+		{
+			return power == 0 ? 1 :
+				power == 1 ? number :
+				number > 0 ? number * Power(number, --power) :
+				1 / Power(number, --power);
 		}
 	}
 }
