2c8df3a53d1f2b117759f920473d0cae7918f842
alkeity <alkeity.23@gmail.com>
2024-06-27 14:03:54 +0300

    Calc classwork

--- Calc2/Program.cs
+++ Calc2/Program.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -50,66 +51,113 @@ namespace Calc2
 			}
 			Main(args); 
 #endif
-			bool isCorrect = true;
-			do
+			string expression = "(22 + 33 * (44 + 55)) / 5";
+			Console.WriteLine($"Result: {Browse(expression)}");
+		}
+
+		static string Browse(string complexExpr)
+		{
+			complexExpr = complexExpr.Replace(" ", "");
+			for (int i = 0; i < complexExpr.Length; i++)
 			{
-				Console.Write("Введите арифметическое выражение: ");
-				string expression = Console.ReadLine();
-				expression = expression.Replace(" ", "");
-				string operations = "+-*/";
-				String[] sNums = expression.Split(operations.ToCharArray());
-				double[] dNums = new double[sNums.Length];
-				sNums = sNums.Where(item => item != "").ToArray();
-				for (int i = 0; i < sNums.Length; i++)
+				if (complexExpr[i] == '(')
 				{
-					dNums[i] = Convert.ToDouble(sNums[i]);
-					Console.Write(dNums[i] + " ");
-				}
-				Console.WriteLine();
-				char[] sOperations = expression.Where(item => operations.Contains(item)).ToArray();
-
-				for (int i = 0; i < sOperations.Length; i++)
-				{
-					Console.Write(sOperations[i] + " ");
-				}
-				Console.WriteLine();
-
-				if (dNums.Length != sOperations.Length + 1)
-				{
-					Console.WriteLine("Выражение некорректно.");
-					continue;
-				}
-
-				for (int i = 0; i < sOperations.Length; i++)
-				{
-					if (sOperations[i] == '*' || sOperations[i] == '/')
+					for (int j = 0; j < complexExpr.Length; j++)
 					{
-						if (sOperations[i] == '*') { dNums[i] *= dNums[i + 1]; }
-						else { dNums[i] /= dNums[i + 1]; }
-
-						for (int j = i; j < dNums.Length - 1; j++)
+						if (complexExpr[j] == ')')
 						{
-							dNums[j] = dNums[j + 1];
+							//string subexpr = complexExpr.Substring(i, j - i);
+							//double subresult = Calculate(subexpr.Substring(1, subexpr.Length - 2));
+							//complexExpr = complexExpr.Replace(subexpr, subresult.ToString());
+							string subexpr = complexExpr.Substring(i + 1, j - i - 1);
+							double subres = Calculate(subexpr);
+							Console.WriteLine(subexpr + " = " + subres);
+							complexExpr = complexExpr.Replace($"({subexpr})", subres.ToString());
+							break;
 						}
-						for (int j = i; j < sOperations.Length - 1; j++)
+						else if (complexExpr[j] == '(')
 						{
-							sOperations[j] = sOperations[j + 1];
+							Browse(complexExpr.Substring(i + 1, complexExpr.Length - i - 1));
 						}
-						dNums[dNums.Length - 1] = 0;
-						sOperations[sOperations.Length - 1] = '\0';
 					}
 				}
-				for (int j = 0; j < dNums.Length; j++)
+			}
+			return complexExpr;
+		}
+
+		static double Calculate(string expression)
+		{
+			expression = expression.Replace(" ", "");
+			Console.WriteLine(expression);
+			string operations = "+-*/";
+			String[] sNums = expression.Split(operations.ToCharArray());
+			double[] dNums = new double[sNums.Length];
+			sNums = sNums.Where(item => item != "").ToArray();
+			for (int i = 0; i < sNums.Length; i++)
+			{
+				Console.WriteLine(sNums[i]);
+				dNums[i] = Convert.ToDouble(sNums[i]);
+				Console.Write(dNums[i] + " ");
+			}
+			Console.WriteLine();
+			char[] sOperations = expression.Where(item => operations.Contains(item)).ToArray();
+
+			for (int i = 0; i < sOperations.Length; i++)
+			{
+				Console.Write(sOperations[i] + " ");
+			}
+			Console.WriteLine();
+
+			if (dNums.Length != sOperations.Length + 1)
+			{
+				throw new Exception("Invalid expression");
+			}
+
+			for (int i = 0; i < sOperations.Length; i++)
+			{
+				while (sOperations[i] == '*' || sOperations[i] == '/')
 				{
-					Console.Write(dNums[j] + " ");
+					if (sOperations[i] == '*') dNums[i] *= dNums[i + 1];
+					else dNums[i] /= dNums[i + 1];
+
+					for (int j = 0; j < dNums.Length; j++)
+					{
+						dNums[j] = dNums[j + 1];
+					}
+
+					for (int j = 0; j < sOperations.Length; j++)
+					{
+						sOperations[j] = sOperations[j + 1];
+					}
+
+					dNums[dNums.Length - 1] = 0;
+					sOperations[sOperations.Length - 1] = '\0';
 				}
-				Console.WriteLine();
-				for (int j = 0; j < sOperations.Length; j++)
+			}
+
+			for (int i = 0; i < sOperations.Length; i++)
+			{
+				while (sOperations[i] == '+' || sOperations[i] == '-')
 				{
-					Console.Write(sOperations[j] + " ");
+					if (sOperations[i] == '+') dNums[i] += dNums[i + 1];
+					else dNums[i] -= dNums[i + 1];
+
+					for (int j = i + 1; j < dNums.Length - 1; j++)
+					{
+						dNums[j] = dNums[j + 1];
+					}
+
+					for (int j = i; j < sOperations.Length - 1; j++)
+					{
+						sOperations[j] = sOperations[j + 1];
+					}
+
+					dNums[dNums.Length - 1] = 0;
+					sOperations[sOperations.Length - 1] = '\0';
 				}
-				Console.WriteLine();
-			} while (!isCorrect);
+			}
+
+			return dNums[0];
 		}
 	}
 }
