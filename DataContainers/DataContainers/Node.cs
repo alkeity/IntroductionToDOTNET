c0a7d0ffc5f4fb6f0b4706dbfa29fff75891fbff
alkeity <alkeity.23@gmail.com>
2024-07-17 02:10:50 +0300

    feat: BaseNode, Node, Stack

---
+++ DataContainers/DataContainers/Node.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DataContainers
+{
+	internal class BaseNode<T>
+	{
+		T value;
+		BaseNode<T> next;
+
+		public T Value
+		{
+			get => value;
+			set
+			{
+				this.value = value;
+			}
+		}
+
+		public BaseNode<T> Next
+		{
+			get => next;
+			set
+			{
+				next = value;
+			}
+		}
+
+        public BaseNode(T value)
+        {
+            Value = value;
+			Next = null;
+        }
+
+		public BaseNode(T value, BaseNode<T> next)
+		{
+			Value = value;
+			Next = next;
+		}
+	}
+
+	internal class Node<T> : BaseNode<T>
+	{
+		Node<T> prev;
+
+		public Node<T> Prev
+		{
+			get => prev;
+			set
+			{
+				prev = value;
+			}
+		}
+
+        public Node(T value) : base(value)
+        {
+            Prev = null;
+        }
+
+		public Node(Node<T> prev, T value) : base(value)
+		{
+			Prev = prev;
+		}
+
+		public Node(T value, Node<T> next) : base(value, next)
+		{
+			Prev = null;
+		}
+
+		public Node(T value, Node<T> next, Node<T> prev) : base(value, next)
+		{
+			Prev = prev;
+		}
+	}
+}
---
+++ DataContainers/DataContainers/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContainers
+{
+	internal class Stack<T>
+	{
+		BaseNode<T> head;
+		int count;
+
+		public BaseNode<T> Head
+		{
+			get => head;
+			set { head = value; }
+		}
+
+		public int Count
+		{
+			get => count;
+			private set { count = value; }
+		}
+
+        public Stack()
+        {
+			Head = null;
+			count = 0;
+        }
+
+		public void AddElement(T value)
+		{
+			BaseNode<T> temp = new BaseNode<T>(value);
+			temp.Next = Head;
+			Head = temp;
+			Count++;
+		}
+
+		public void RemoveElement()
+		{
+			BaseNode<T> temp = Head;
+			Head = temp.Next;
+			Count--;
+		}
+
+		public BaseNode<T> GetElement()
+		{
+			BaseNode<T> temp = Head;
+			RemoveElement();
+			return temp;
+		}
+
+		public void Clear()
+		{
+			while (count > 0)
+			{
+				RemoveElement();
+			}
+		}
+    }
+}
