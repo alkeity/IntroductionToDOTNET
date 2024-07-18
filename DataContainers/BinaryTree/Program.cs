3d53870e405bcb5cf38f3915c2ced4158fea6cf5
alkeity <alkeity.23@gmail.com>
2024-07-18 17:30:28 +0300

    classwork: Tree

---
+++ DataContainers/BinaryTree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rand = new Random();
+			Console.Write("Введите размер дерева: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			Tree tree = new Tree();
+
+			for (int i = 0; i < n; i++)
+			{
+				tree.Insert(rand.Next(100), tree.root);
+			}
+			tree.Print(tree.root);
+		}
+	}
+}
---
+++ DataContainers/BinaryTree/Tree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+	internal class Tree
+	{
+		public class Element
+		{
+			public int data;
+			public Element pLeft;
+			public Element pRight;
+
+			public Element(int data, Element pLeft = null, Element pRight = null)
+			{
+				this.data = data;
+				this.pLeft = pLeft;
+				this.pRight = pRight;
+				Console.WriteLine($"Elem ctor: {GetHashCode()}");
+			}
+
+			~Element()
+			{
+				Console.WriteLine($"Elem dtor: {GetHashCode()}");
+			}
+		}
+		public Element root;
+
+        public Tree()
+        {
+			Console.WriteLine($"Tree ctor: {GetHashCode()}");
+        }
+
+		~Tree()
+		{
+			Console.WriteLine($"Tree dtor: {GetHashCode()}");
+		}
+
+		public void Insert(int data, Element root)
+		{
+			if (this.root == null) this.root = new Element(data);
+			if (root == null) return;
+			if (data < root.data)
+			{
+				if (root.pLeft == null) root.pLeft = new Element(data);
+				else Insert(data, root.pLeft);
+			}
+			else
+			{
+				if (root.pRight == null) root.pRight = new Element(data);
+				else Insert(data, root.pRight);
+			}
+		}
+
+		public void Print(Element root)
+		{
+			if (root == null) return;
+			Print(root.pLeft);
+			Console.WriteLine(root.data + " ");
+			Print(root.pRight);
+		}
+	}
+}
